d2424d0ad61997582dcf328bccdcb4f1f284f490
Subhasis <subhasis.jena@vertisinfotech.com>
2016-07-08 12:29:29 +0530

    Modified the test file, uncommented

--- sort.py
+++ sort.py
@@ -33,14 +33,14 @@
 # prime_numbers(list)
 
 
-# num = input('get the value:')
-# for i in range(2,num+1):
-#     count = 0
-#     for j in range(2,i):
-#         if i%j != 0:
-#             count += 1
-#     if count == i-2:
-#         print i,
+num = input('get the value:')
+for i in range(2,num+1):
+    count = 0
+    for j in range(2,i):
+        if i%j != 0:
+            count += 1
+    if count == i-2:
+        print i,
 
 # def prime_number(a):
 #     yes=[]
